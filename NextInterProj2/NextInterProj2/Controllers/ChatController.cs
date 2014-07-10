57f468c869d7a46fe47d49ec269932251ca7297b
SanyaBogos <bog_alex_ur@ukr.net>
2014-07-11 00:18:45 +0300

    Try to find Current user id

--- NextInterProj2/NextInterProj2/Controllers/ChatController.cs
+++ NextInterProj2/NextInterProj2/Controllers/ChatController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using NextInterProj2.Models;
 using System.Data.Entity;
+using Microsoft.AspNet.Identity;
 
 namespace NextInterProj2.Controllers
 {
@@ -14,11 +15,32 @@ namespace NextInterProj2.Controllers
         // GET: /Chat/
         UsersContext db = new UsersContext();
 
+        [Authorize(Roles = "admin")]
         public ActionResult Index()
         {
-            var messages = db.Chats.Include(m => m.Sender);
+            var messages = db.Chats.Include(m => m.Reciever);
+            messages = messages.Include(n => n.Sender);
             return View(messages.ToList());
         }
 
+        [Authorize]
+        public ActionResult Message()
+        {
+            var people = db.UserProfiles;
+            return View(people.ToList());
+        }
+
+        [Authorize]
+        public ActionResult Say(int? recieverId)
+        {
+            //if (recieverId == null)
+            //    return HttpNotFound();
+            int sendId = (int)WebMatrix.WebData.WebSecurity.CurrentUserId;
+
+            int senderId = Convert.ToInt32(User.Identity.GetUserId());
+            var messages = db.Chats.Include(m => m.Reciever).Include(n => n.Sender).Where(s => s.SenderUserId == senderId
+                && s.RecieverUserId == recieverId);
+            return View(messages.ToList());
+        }
     }
 }
